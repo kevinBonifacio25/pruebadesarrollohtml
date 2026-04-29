3bf1e54f3b5a31483dd5d8a87465eca188400eb6
kevinBonifacio25 <127898634+kevinBonifacio25@users.noreply.github.com>
2026-04-29 07:22:53 +0000

    actualizacion

--- performance-test-M2-main/assets/js/main.js
+++ performance-test-M2-main/assets/js/main.js
@@ -1,5 +1,55 @@
-toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
 
-    toggle.textContent = nav.classList.contains("active") ? "✖" : "☰";
+const track = document.querySelector('.carousel__track');
+const slides = document.querySelectorAll('.carousel__track img');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let index = 0;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    index++;
+    if (index >= slides.length) {
+        index = 0; // vuelve al inicio
+    }
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1; // va al final
+    }
+    updateCarousel();
+});
+
+
+
+//<!-- javaScript del menú hamburguesa -->//
+document.addEventListener("DOMContentLoaded", () => { 
+
+    const navList = document.querySelector(".nav__list"); //<!--selecciona el elemento de la lista de navegación-->//
+    const toggle = document.querySelector(".menu-toggle"); //<!--selecciona el elemento del botón de menú-->//
+
+    toggle.addEventListener("click", () => {
+
+        navList.classList.toggle("active");
+
+        toggle.textContent = navList.classList.contains("active")
+            ? "✖"
+            : "☰";
+    });
+
+});
+
+
+//<!-- cierra el menú al hacer click en un enlace -->//
+document.querySelectorAll(".nav__list a").forEach(link => {
+    link.addEventListener("click", () => {
+        navList.classList.remove("active"); //<!--remueve el estado activo del menú y lo cierra-->//
+        toggle.textContent = "☰";
+    });
 });
